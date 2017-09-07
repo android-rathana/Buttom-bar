bf059987fec86fde55e8d95b7d358df03e1561cf
rathanaVoy <rathanavoy07@gmail.com>
2017-09-07 17:54:31 +0700

    add searchView

---
+++ app/src/main/java/com/khendec/rathana/kh_en_dectionary/event/FavoritePopupMenuEvent.java
@@ -0,0 +1,8 @@
+package com.khendec.rathana.kh_en_dectionary.event;
+
+/**
+ * Created by ratha on 07-Sep-17.
+ */
+
+public class FavoritePopupMenuEvent {
+}
---
+++ app/src/main/java/com/khendec/rathana/kh_en_dectionary/event/message/SnackbarMessage.java
@@ -0,0 +1,8 @@
+package com.khendec.rathana.kh_en_dectionary.event.message;
+
+/**
+ * Created by ratha on 07-Sep-17.
+ */
+
+public class SnackbarMessage {
+}
